33016a8a34a74ff494e67c9e630b41ae707192f9
code-craz <doohickycool@gmail.com>
2023-01-21 14:31:40 +0530

    Update Page 2.py

--- Page 2.py
+++ Page 2.py
@@ -10,20 +10,30 @@ root.title("Code fest health bot")
 
 def emergency():
     new_win= Toplevel(root)
-    new_win.title("Health bot emergenc stage")
+    new_win.title("Health bot emergency stage")
 
     var=StringVar
     Label(new_win, text="Is your emergency any of the folowing?").pack()
+    Checkbutton(new_win, text="Option 1", variable=var, command=lambda:print("U ded")).pack()
+    Checkbutton(new_win, text="Option 2", variable=var, command=lambda:print("U still ded")).pack()
+
+#Create chronic function
+def chronic():
+    #Create a new window
+    new_win=Toplevel(root)
+    new_win.title("Health bot chronic stage")
+    #Ask for issue
+    Label(new_win, text="Is your issue any of the folowing?").pack()
+    var=StringVar
     Checkbutton(new_win, text="Option 1", variable=var, command=lambda:print("Sup")).pack()
     Checkbutton(new_win, text="Option 2", variable=var, command=lambda:print("Bro")).pack()
 
-#
 #Check if user is in an emergency. (Using buttons for check)
 emcheck= Label(root, text= "Are you in an emergency?")
 y= Button(root, text="Yes", command=emergency)
 space= Label(root, text="      ")
-n= Button(root, text="No")
-
+n= Button(root, text="No", command=chronic)
+#Place the checks and buttons
 emcheck.grid(row=1, column=2)
 y.grid(row=2, column=1)
 space.grid(row=3, column=1)
